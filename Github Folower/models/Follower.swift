57f116b11b50059cb22c029208a93cb072dbce1b
KerollosNabil <kokonabil4444@gmail.com>
2020-11-05 12:20:17 +0200

    add follower and user model

---
+++ Github Folower/models/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  Github Folower
+//
+//  Created by MAC on 11/5/20.
+//
+
+import Foundation
+
+struct Follower:Codable {
+    var login:String
+    var avatarUrl:String
+}
