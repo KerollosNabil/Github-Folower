1da71ba0effeb3ecfd14e1eee3c02e3fdce92207
KerollosNabil <kokonabil4444@gmail.com>
2020-11-05 12:23:40 +0200

    update project structer

---
+++ Github Folower/models/User.swift
@@ -0,0 +1,24 @@
+//
+//  User.swift
+//  Github Folower
+//
+//  Created by MAC on 11/5/20.
+//
+
+import Foundation
+
+struct User:Codable {
+    
+    var login:String
+    var avatarUrl:String
+    var name:String?
+    var bio:String?
+    var location:String?
+    var publicRepos:Int
+    var publicGists:Int
+    var htmlUrl:Int
+    var followers:Int
+    var following:Int
+    var createdAt:String
+    
+}
